df216eaf80f304e518afee7cc405ce96eb44f9db
Qiao Xu <sleeepd@gmail.com>
2025-09-26 00:53:06 -0600

    Add Load Level function in Game Manager
    + Can start the game from the main menu
    + Add a fade effect while loading a scene

--- Alberta_GameJam/Assets/Scripts/Core/GameManager.cs
+++ Alberta_GameJam/Assets/Scripts/Core/GameManager.cs
@@ -1,9 +1,38 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
+    UIFadeEffect _fadeEffect;
+    
     public override void Awake()
     {
         base.Awake();
+        _fadeEffect = GetComponentInChildren<UIFadeEffect>();
+    }
+
+    public void StartGame()
+    {
+        _fadeEffect.FadeIn(() => LoadScene("Level01"));
+    }
+
+    void LoadScene(string levelName)
+    {
+        StartCoroutine(LoadSceneAsync(levelName, OnLoaded));
+    }
+
+    IEnumerator LoadSceneAsync(string levelName, Action onComplete)
+    {
+        var op = SceneManager.LoadSceneAsync(levelName);
+        while (!op.isDone)
+            yield return null;
+        onComplete?.Invoke();
+    }
+
+    void OnLoaded()
+    {
+        _fadeEffect.FadeOut();
     }
 }
