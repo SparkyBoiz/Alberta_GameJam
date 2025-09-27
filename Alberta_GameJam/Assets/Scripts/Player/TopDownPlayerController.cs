22b95e00dac6ddfb6c91b77684db363d6e1f8960
Qiao Xu <sleeepd@gmail.com>
2025-09-27 11:33:20 -0600

    Added Player Sound Effect

--- Alberta_GameJam/Assets/Scripts/Player/TopDownPlayerController.cs
+++ Alberta_GameJam/Assets/Scripts/Player/TopDownPlayerController.cs
@@ -6,16 +6,27 @@ namespace Game.Player
     [RequireComponent(typeof(Rigidbody2D))]
     public class TopDownPlayerController : MonoBehaviour
     {
+        public enum State
+        {
+            Idle,
+            Moving
+        }
         public float moveSpeed = 6f;
         public Game.Core.InputSystem_Actions inputActions;
         private Rigidbody2D _rb;
         private Vector2 _moveInput;
+        private SoundWord _soundWord;
+        private Animator _animator;
+        public State state { get; private set; }
 
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
             _rb.gravityScale = 0f;
             _rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+
+            _soundWord = GetComponentInChildren<SoundWord>();
+            _animator = GetComponentInChildren<Animator>();
 
 
             if (inputActions == null)
@@ -29,22 +40,83 @@ namespace Game.Player
         {
             inputActions.Enable();
             inputActions.Player.Move.performed += OnMove;
-            inputActions.Player.Move.canceled += OnMove;
+            inputActions.Player.Move.canceled += OnCancelMove;
         }
 
         private void OnDisable()
         {
             inputActions.Player.Move.performed -= OnMove;
-            inputActions.Player.Move.canceled -= OnMove;
+            inputActions.Player.Move.canceled -= OnCancelMove;
             inputActions.Disable();
         }
 
         private void OnMove(InputAction.CallbackContext ctx)
         {
             _moveInput = ctx.ReadValue<Vector2>();
+            EnterMoving();
+        }
+
+        private void OnCancelMove(InputAction.CallbackContext ctx)
+        {
+            _moveInput = Vector2.zero;
+            EnterIdle();
+        }
+
+        void Update()
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    HandleIdle();
+                    break;
+                case State.Moving:
+                    HandleMoving();
+                    break;
+            }
         }
 
         private void FixedUpdate()
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    HandleIdlePhysics();
+                    break;
+                case State.Moving:
+                    HandleMovingPhysics();
+                    break;
+            }
+        }
+
+        void EnterIdle()
+        {
+            _rb.linearVelocity = Vector3.zero;
+            state = State.Idle;
+            _animator.SetBool("Moving", false);
+        }
+
+        void EnterMoving()
+        {
+            state = State.Moving;
+            _animator.SetBool("Moving", true);
+        }
+
+        void HandleIdle()
+        {
+
+        }
+
+        void HandleIdlePhysics()
+        {
+
+        }
+
+        void HandleMoving()
+        {
+            _soundWord.Spawn(transform.position, Vector3.up, 2f);
+        }
+
+        void HandleMovingPhysics()
         {
             // WASD movement
             _rb.linearVelocity = _moveInput * moveSpeed;
@@ -58,7 +130,6 @@ namespace Game.Player
             {
                 transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
             }
-
         }
     }
 }
