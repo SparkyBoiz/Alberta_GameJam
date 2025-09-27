e0e1fbfcf759787cac72daf5ee56c87893eca3f9
Qiao Xu <sleeepd@gmail.com>
2025-09-27 01:32:20 -0600

    Added Enemy with Sound Effect

---
+++ Alberta_GameJam/Assets/Scripts/Enemy/Ghost.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ghost : MonoBehaviour
+{
+    [SerializeField] float moveRadius;
+    [SerializeField] float idleDuration = 3f;
+    public enum State
+    {
+        Idle,
+        Patrol
+    }
+
+    NavMeshAgent agent;
+    float idleTimer;
+    State state;
+    SoundWord soundEffect;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        soundEffect = GetComponentInChildren<SoundWord>();
+        EnterIdle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                HandleIdle();
+                break;
+            case State.Patrol:
+                HandlePatrol();
+                break;
+        }
+    }
+
+    void EnterIdle()
+    {
+        state = State.Idle;
+        idleTimer = idleDuration;
+        if (agent != null)
+        {
+            agent.ResetPath();
+        }
+    }
+
+    void HandleIdle()
+    {
+        if (idleTimer > 0f)
+        {
+            idleTimer -= Time.deltaTime;
+        }
+
+        if (idleTimer <= 0f)
+        {
+            EnterPatrol();
+        }
+    }
+
+    void EnterPatrol()
+    {
+        state = State.Patrol;
+        FindNextWaypoint();
+    }
+
+    void HandlePatrol()
+    {
+        soundEffect.Spawn(transform.position, Vector3.up, 1f);
+        if (agent == null || agent.pathPending)
+        {
+            return;
+        }
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            if (!agent.hasPath || agent.velocity.sqrMagnitude <= 0.01f)
+            {
+                EnterIdle();
+            }
+        }
+    }
+
+    void FindNextWaypoint()
+    {
+        if (agent == null || !agent.isActiveAndEnabled || !agent.isOnNavMesh)
+        {
+            return;
+        }
+
+        var origin = transform.position;
+        const int maxAttempts = 10;
+        for (int attempt = 0; attempt < maxAttempts; attempt++)
+        {
+            var randomDirection = Random.insideUnitSphere * moveRadius;
+            var target = origin + randomDirection;
+            if (NavMesh.SamplePosition(target, out var navHit, moveRadius, NavMesh.AllAreas))
+            {
+                var path = new NavMeshPath();
+                if (agent.CalculatePath(navHit.position, path) && path.status == NavMeshPathStatus.PathComplete)
+                {
+                    agent.SetDestination(navHit.position);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- Alberta_GameJam/Assets/Scripts/Sound/SoundWord.cs
+++ Alberta_GameJam/Assets/Scripts/Sound/SoundWord.cs
@@ -6,43 +6,33 @@ public class SoundWord : MonoBehaviour
     VisualEffect visualEffect;
     [SerializeField] Texture2D texture;
     [SerializeField] float speed;
-    Vector3 position;
-    Vector3 direction;
-    public bool isPlaying { get; private set; }
+    [SerializeField] float playInterval = 0.2f;
+    float nextPlayTime;
 
     void Awake()
     {
         visualEffect = GetComponent<VisualEffect>();
-        isPlaying = false;
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
         visualEffect.SetTexture("Texture", texture);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void Play()
-    {
-        isPlaying = true;
-    }
-
-    public void Stop()
-    {
-        isPlaying = false;
+        nextPlayTime = 0f;
     }
 
     public void Spawn(Vector3 position, Vector3 direction, float size)
     {
+        Debug.Log("Spawn!");
+        if (playInterval > 0f && Time.time < nextPlayTime)
+        {
+            return;
+        }
+
+        if (playInterval > 0f)
+        {
+            nextPlayTime = Time.time + playInterval;
+        }
+
         visualEffect.SetVector3("Position", position);
         visualEffect.SetVector3("Direction", direction);
         visualEffect.SetFloat("Size", size);
         visualEffect.SetFloat("Speed", speed);
+        visualEffect.SendEvent("OnPlay");
     }
 }
